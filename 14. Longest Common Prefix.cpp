24fbec6c779ceabc108cbdb6d1891aa078fa91de
madhav <109799001+madhav037@users.noreply.github.com>
2023-05-14 17:41:39 +0530

    prettier

--- 14. Longest Common Prefix.cpp
+++ 14. Longest Common Prefix.cpp
@@ -1,17 +1,16 @@
 /*Write a function to find the longest common prefix string amongst an array of strings.
 
 If there is no common prefix, return an empty string "".*/
-#include<iostream>
-#include<string.h>
+#include <iostream>
+#include <string.h>
 
 using namespace std;
 
-int main() {
-    int strsSize = 4
-    string strs[strsSize] = {"flower", "floor", "flour", "fool"};
-    //0->1->2->...
-    //f->?
-    //fl->?
-    //flo->?
-    
+int main()
+{
+    int strsSize = 4 string strs[strsSize] = {"flower", "floor", "flour", "fool"};
+    // 0->1->2->...
+    // f->?
+    // fl->?
+    // flo->?
 }
